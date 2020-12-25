cdeb4264ddaf5345fd8571da53c277524f263337
Alberto Biasao <biasao@gmail.com>
2020-12-25 18:51:09 +0100

    initial commit

---
+++ Prototype 1/Assets/Challenge 1/Scripts/SpinPropeller.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpinPropeller : MonoBehaviour
+{
+    private float speed = 1000f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Rotate(Vector3.forward, speed * Time.deltaTime);
+    }
+}
